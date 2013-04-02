9a10afc17f276b76e7428f7ea3c8888b2e9e9cfd
Aidan Feldman <aidan.feldman@gmail.com>
2013-04-01 21:42:11 -0400

    check that the thumbnail exists

--- test/live_tests.js
+++ test/live_tests.js
@@ -1,7 +1,8 @@
 var fs = require('fs'),
   oembed = require('../lib/oembed'),
   u = require('underscore'),
-  assert = require('assert');
+  assert = require('assert'),
+  request = require('request');
 
 describe('Live tests', function(){
   var examples = JSON.parse(fs.readFileSync('test/examples.json'));
@@ -9,16 +10,23 @@ describe('Live tests', function(){
   u.each(examples, function(expected, url){
     it("should succeed for " + url, function(done){
       oembed.fromUrl(url, function(err, result){
-        if (!err){
+        if (err){
+          done(err);
+        } else {
           assert(!u.isEmpty(result), "should not be empty");
 
           // check that example data is subset of the response
           u.each(expected, function(val, key){
             assert.strictEqual(result[key], val, key + "should match");
           });
-        }
 
-        done(err);
+          request({
+            url: result.thumbnail_url,
+            method: 'HEAD'
+          }, function(thumbErr){
+            done(thumbErr);
+          });
+        }
       });
     });
   });
