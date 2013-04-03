3648d90c491badb81bfd841c5ed4754a3feaaac8
Aidan Feldman <aidan.feldman@gmail.com>
2013-04-03 04:03:53 -0400

    split app into separate module

--- server.js
+++ server.js
@@ -1,24 +1,4 @@
-var express = require('express'),
-  oembed = require('./lib/oembed');
-
-var app = express();
-
-app.use(express.logger());
-app.use(express.compress());
-
-app.get('/v1.json', function(req, res){
-  oembed.fromUrl(req.query.url, function(err, json){
-    if (err){
-      res.json(404, {
-        status: 404,
-        message: err
-      });
-    } else {
-      res.json(json);
-    }
-  });
-});
-
+var app = require('./lib/app.js');
 
 var port = process.env.PORT || 3000;
 app.listen(port);
